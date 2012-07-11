ec341c5bd2716fda39062681879b88ab893d230c
Michael Kaiser-Nyman + Jeremy Reyes <apprentice+mk+jr@devbootcamp.com>
2012-07-11 16:25:45 -0700

    added database backend to template

--- template.rb
+++ template.rb
@@ -1,10 +1,18 @@
 module CraigslistCrawler
-
-  attr_accessor :text
-
   class Template
+    attr_accessor :text
     def initialize(text)
       @text = text
+    end
+
+    def save
+      CraigslistCrawler.database.execute("INSERT INTO message_templates (text) VALUES (:text)", :text => @text)
+      CraigslistCrawler.database.last_insert_row_id
+    end
+
+    def self.from_db(template_id)
+      template_array = CraigslistCrawler.database.execute("SELECT text FROM message_templates WHERE id = #{template_id}")
+      Template.new(template_array[0][0])
     end
   end
 end
--- template_spec.rb
+++ template_spec.rb
@@ -1,10 +1,15 @@
 require "./spec_helper"
 
-include CraigslistCrawler
-
 describe Template do
 
   let(:test_template) { Template.new("Hey you've got a great place. Let's talk.") }
+
+  before :each do
+    CraigslistCrawler.database.execute("DROP TABLE IF EXISTS message_templates")
+    CraigslistCrawler.database.execute("CREATE TABLE message_templates ( id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                      text TEXT NOT NULL
+                                     );")
+  end
 
   context "#new" do
     it "initializes as a new template" do
@@ -22,6 +27,19 @@ describe Template do
     it "changes the text of the template" do
       test_template.text= "This is a great new message."
       test_template.text.should eq("This is a great new message.")
+    end
+  end
+
+  context "#save" do
+    it "should return the database id for the template" do
+      test_template.save.should eq CraigslistCrawler.database.last_insert_row_id
+    end
+  end
+
+  context ".from_db" do
+    it "should create a template from the database" do
+      template_id = test_template.save
+      Template.from_db(template_id).text.should eq "Hey you've got a great place. Let's talk."
     end
   end
 
